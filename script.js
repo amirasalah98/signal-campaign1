f46565584fdfde7d8ef26e4ecc1031bb9d37cc3d
amirasalah98 <78090962+amirasalah98@users.noreply.github.com>
2023-08-15 17:38:50 +0300

    add

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ signupText.style.display='block'
 wheelContainer.appendChild(signupText);
 
 // wheel color after submit
-var wheel=document.querySelector('.wheel')
-var wheelItem= document.querySelector('.wheel-item::before')
+var wheel=document.getElementsByClassName('wheel')
+var wheelItem= document.getElementsByClassName('wheel-item::before')
 var form=document.getElementsByTagName('form')
 console.log(wheel);
 console.log(wheelItem);
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    wheel.classList.add("wheelColor");
-    wheel.classList.remove("wheel");
-    wheelItem.classList.add("wheelColor");
+// form.addEventListener("submit", () => {
+    // wheelItem.setAttribute('style', 'color:black !important');
+
+//     e.preventDefault()
+//     wheel.classList.add("wheelColor");
+//     wheelItem.classList.add("wheelColor");
     console.log(wheel);
     console.log(wheelItem);
-})
+// })
+// wheel.style.color="#000";
+    // wheelItem[0].style.backgroundColor="#000";
+    // wheelItem.setAttribute('style', 'color:black !important');
+    // document.head.innerHTML += '<style> .wheel { color: #fff `!important` } </style>';
