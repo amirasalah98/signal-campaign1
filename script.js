0d6b6fd1ededee9e0c84c97f46aebe33756204a5
amirasalah98 <78090962+amirasalah98@users.noreply.github.com>
2023-08-15 14:40:00 +0300

    add new

---
+++ script.js
@@ -0,0 +1,6 @@
+const signupText = document.querySelector(".signup-text");
+const wheelContainer=document.querySelector("#form-widget")
+signupText.style.display='block'
+wheelContainer.appendChild(signupText);
+
+// document.body.innerHTML = newSec// document.getElementById("myList").appendChild(node);
