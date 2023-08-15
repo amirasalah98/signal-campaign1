4caf07d11a31264fd53481a8ff7404fcaf084c56
amirasalah98 <78090962+amirasalah98@users.noreply.github.com>
2023-08-15 16:33:25 +0300

    add new

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+//append signup text to the form section
 const signupText = document.querySelector(".signup-text");
 const wheelContainer=document.querySelector("#form-widget")
 signupText.style.display='block'
 wheelContainer.appendChild(signupText);
 
-// document.body.innerHTML = newSec// document.getElementById("myList").appendChild(node);
+// wheel color after submit
+var wheelItem= document.getElementsByClassName('wheel-item::before')
+var wheel=document.getElementsByClassName('wheel')
+var form=document.getElementsByTagName('form')
+console.log(wheel);
+console.log(wheelItem);
+
+form.addEventListener("submit", () => {
+    wheel.classList.add("wheelColor");
+    wheelItem.classList.add("wheelColor");
+    console.log(wheel);
+    console.log(wheelItem);
+})
